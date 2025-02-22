f4180497cef976a8105f5d5852f96673a1238ff4
Pham Nhu Quoc Trieu <phamnhuquoctrieu307@gmail.com>
2025-02-22 15:31:10 +0700

    Feature-header #3

---
+++ src/view/components/header.js
@@ -0,0 +1,21 @@
+import searchBar from "./searchBar";
+import iconAndNotification from "./iconAndNotifiaction";
+
+import { calendar, bell, notification, noAvt, noImg, envelop, divider, down } from "./../../assets/icon";
+import avatarUser from "./avatarUser";
+
+const header = () => {
+    return ` 
+    <div class="header">
+        ${searchBar('Search')}
+                ${iconAndNotification(calendar, 6)}   
+                ${iconAndNotification(bell, 8)}
+                ${iconAndNotification(envelop, 9)}
+                ${iconAndNotification(noImg, 0)}
+                <img src="${divider}" alt="divider" class="header-divider">
+                ${avatarUser()}
+    </div>     
+    `;
+}
+
+export default header; 
---
+++ src/view/components/iconAndNotifiaction.js
@@ -0,0 +1,13 @@
+const iconAndNotification = ( src , notification) => {
+     return `
+         <div class = "iconAndNotification">
+             <img src=${src} alt="icon " class= " iconAndNotification-image" />
+             ${notification > 0 ? `
+              <div class = "iconAndNotification-notification">
+                <p class = "iconAndNotification-notification-number">${notification}</p>
+             </div>` : ''}     
+         </div>
+     `
+}
+ 
+export default iconAndNotification;      
